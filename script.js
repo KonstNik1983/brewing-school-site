80974410473750462df765a1f86ae26f1f5cebef
KonstNik1983 <nikolaev.6884@gmail.com>
2025-02-06 20:23:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper(".mySwiper", {
+  spaceBetween: 50,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  keyboard: true,
+});
